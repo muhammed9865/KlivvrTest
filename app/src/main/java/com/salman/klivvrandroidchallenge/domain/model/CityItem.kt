05abc523e931d4584a406f8ad92c4356f3ee16ce
Muhammed Salman <mahmadslman@gmail.com>
2025-05-23 22:11:41 +0300

    add City domain models

---
+++ app/src/main/java/com/salman/klivvrandroidchallenge/domain/model/CityItem.kt
@@ -0,0 +1,18 @@
+package com.salman.klivvrandroidchallenge.domain.model
+
+/**
+ * Created by Muhammed Salman email(mahmadslman@gmail.com) on 5/23/2025.
+ *
+ * Domain model represents a City item.
+ *
+ * Notes
+ * - [searchKey] is used to sort and search the list of cities.
+ */
+data class CityItem(
+    val id: Int,
+    val name: String,
+    val country: String,
+    val image: ImageResource,
+    val coordinates: Coordinates,
+    val searchKey: String,
+)
---
+++ app/src/main/java/com/salman/klivvrandroidchallenge/domain/model/Coordinates.kt
@@ -0,0 +1,9 @@
+package com.salman.klivvrandroidchallenge.domain.model
+
+/**
+ * Created by Muhammed Salman email(mahmadslman@gmail.com) on 5/23/2025.
+ */
+data class Coordinates(
+    val latitude: Double,
+    val longitude: Double,
+)
