4becfeb7735304ccf748e2940d6d84481d07af37
Muhammed Salman <mahmadslman@gmail.com>
2025-05-25 16:14:01 +0300

    refactor: remove unused import in SearchBar.kt

--- app/src/main/java/com/salman/klivvrandroidchallenge/presentation/composable/SearchBar.kt
+++ app/src/main/java/com/salman/klivvrandroidchallenge/presentation/composable/SearchBar.kt
@@ -38,7 +38,6 @@ import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.platform.LocalLayoutDirection
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.ImeAction
-import androidx.compose.ui.text.style.LineHeightStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.LayoutDirection
 import androidx.compose.ui.unit.dp
