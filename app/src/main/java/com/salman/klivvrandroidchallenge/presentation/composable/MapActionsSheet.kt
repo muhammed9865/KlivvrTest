083a04a3803c8e5c76dd7cd52bd178a38cd83e15
Muhammed Salman <mahmadslman@gmail.com>
2025-05-25 10:17:46 +0300

    feat: replace Image with Icon in MapActionsSheet for improved vector support and layout adjustments

--- app/src/main/java/com/salman/klivvrandroidchallenge/presentation/composable/MapActionsSheet.kt
+++ app/src/main/java/com/salman/klivvrandroidchallenge/presentation/composable/MapActionsSheet.kt
@@ -1,14 +1,13 @@
 package com.salman.klivvrandroidchallenge.presentation.composable
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.SheetState
@@ -17,12 +16,12 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.material3.rememberStandardBottomSheetState
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.salman.klivvrandroidchallenge.domain.model.CityItem
@@ -52,7 +51,8 @@ fun MapActionsSheet(
     ) {
         Row(
             modifier = Modifier.fillMaxWidth()
-                .padding(16.dp)
+                .padding(horizontal = 16.dp)
+                .padding(bottom = 8.dp)
             ,
             horizontalArrangement = Arrangement.SpaceAround,
         ) {
@@ -87,9 +87,10 @@ private fun MapActionCard(
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.spacedBy(8.dp)
     ) {
-        Image(
-            painter = painterResource(mapAction.iconResId),
+        Icon(
+            imageVector = ImageVector.vectorResource(mapAction.iconResId),
             contentDescription = stringResource(mapAction.titleResId),
+            tint = MaterialTheme.colorScheme.onSurface
         )
         Text(
             text = stringResource(mapAction.titleResId),
